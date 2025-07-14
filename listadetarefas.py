37274833431b9e077c208729073db5ef87dd72ca
Manteiga <rafael.manteiga@hotmail.com>
2025-07-13 22:58:40 -0300

    Add files via upload

--- listadetarefas.py
+++ listadetarefas.py
@@ -2,70 +2,120 @@ import liblistadetarefas
 
 listadetarefas = []
 
-while True:
-    print("\nEscolha a opção desejada:")
+def menu_gerenciar():
+    print("\nGerenciar Tarefas:")
     print("1 - Adicionar")
     print("2 - Remover")
-    print("3 - Exibir")
+    print("3 - Editar")
     print("4 - Marcar como concluído")
-    print("5 - Remover concluídos")
-    print("6 - Editar")
-    print("7 - Ordenar alfabeticamente")
-    print("8 - Contar tarefas")
-    print("9 - Exibir tarefas concluídas")
-    print("10 - Exibir tarefas pendentes")
-    print("11 - Salvar tarefas")
-    print("12 - Carregar tarefas")
-    print("13 - Encerrar")
+    print("5 - Remover concluídas")
+
+def menu_exibir():
+    print("\nExibir Tarefas:")
+    print("1 - Todas")
+    print("2 - Concluídas")
+    print("3 - Pendentes")
+    print("4 - Contar tarefas")
+
+def menu_organizar():
+    print("\nOrganizar Tarefas:")
+    print("1 - Ordenar alfabeticamente")
+
+def menu_arquivo():
+    print("\nArquivo:")
+    print("1 - Salvar")
+    print("2 - Carregar")
+
+while True:
+    print("\nMenu Principal:")
+    print("1 - Gerenciar Tarefas")
+    print("2 - Exibir Tarefas")
+    print("3 - Organizar Tarefas")
+    print("4 - Arquivo")
+    print("5 - Encerrar")
 
     try:
-        op = int(input())
+        op = int(input("Escolha a opção desejada: "))
     except ValueError:
         print("Digite um número válido.")
         continue
 
     if op == 1:
-        tarefa = input("\nTarefa que deseja adicionar: ")
-        liblistadetarefas.adicionar(listadetarefas, tarefa)
+        menu_gerenciar()
+        try:
+            subop = int(input("Escolha a opção: "))
+        except ValueError:
+            print("Digite um número válido.")
+            continue
+
+        if subop == 1:
+            tarefa = input("Tarefa que deseja adicionar: ")
+            liblistadetarefas.adicionar(listadetarefas, tarefa)
+        elif subop == 2:
+            try:
+                indice = int(input("Índice da tarefa que deseja remover: ")) - 1
+                liblistadetarefas.remover(listadetarefas, indice)
+            except ValueError:
+                print("Digite um número válido.")
+        elif subop == 3:
+            try:
+                indice = int(input("Índice da tarefa que deseja editar: ")) - 1
+                novatarefa = input("Nova descrição da tarefa: ")
+                liblistadetarefas.editar(listadetarefas, indice, novatarefa)
+            except ValueError:
+                print("Digite um número válido.")
+        elif subop == 4:
+            try:
+                indice = int(input("Índice da tarefa que deseja concluir: ")) - 1
+                liblistadetarefas.concluir(listadetarefas, indice)
+            except ValueError:
+                print("Digite um número válido.")
+        elif subop == 5:
+            liblistadetarefas.removerconcluidos(listadetarefas)
+
     elif op == 2:
+        menu_exibir()
         try:
-            indice = int(input("\nÍndice da tarefa que deseja remover: ")) - 1
+            subop = int(input("Escolha a opção: "))
         except ValueError:
             print("Digite um número válido.")
             continue
-        liblistadetarefas.remover(listadetarefas, indice)
+
+        if subop == 1:
+            liblistadetarefas.mostrar(listadetarefas)
+        elif subop == 2:
+            liblistadetarefas.tconcluidas(listadetarefas)
+        elif subop == 3:
+            liblistadetarefas.tpendentes(listadetarefas)
+        elif subop == 4:
+            liblistadetarefas.qtd(listadetarefas)
+
     elif op == 3:
-        liblistadetarefas.mostrar(listadetarefas)
+        menu_organizar()
+        try:
+            subop = int(input("Escolha a opção: "))
+        except ValueError:
+            print("Digite um número válido.")
+            continue
+
+        if subop == 1:
+            liblistadetarefas.ordenar(listadetarefas)
+
     elif op == 4:
+        menu_arquivo()
         try:
-            indice = int(input("\nÍndice da tarefa que deseja marcar como concluída: ")) - 1
+            subop = int(input("Escolha a opção: "))
         except ValueError:
             print("Digite um número válido.")
             continue
-        liblistadetarefas.concluir(listadetarefas, indice)
+
+        if subop == 1:
+            liblistadetarefas.salvararq(listadetarefas)
+        elif subop == 2:
+            liblistadetarefas.carregararq(listadetarefas)
+
     elif op == 5:
-        liblistadetarefas.removerconcluidos(listadetarefas)
-    elif op == 6:
-        try:
-            indice = int(input("\nÍndice da tarefa que deseja editar: ")) - 1
-            novatarefa = input("\nDigite a tarefa conforme deseja: ")
-        except ValueError:
-            print("Digite um número válido.")
-            continue
-        liblistadetarefas.editar(listadetarefas,indice,novatarefa)
-    elif op == 7:
-        liblistadetarefas.ordenar(listadetarefas)
-    elif op == 8:
-        liblistadetarefas.qtd(listadetarefas)
-    elif op == 9:
-        liblistadetarefas.tconcluidas(listadetarefas)
-    elif op == 10:    
-        liblistadetarefas.tpendentes(listadetarefas)
-    elif op == 11:
-        liblistadetarefas.salvararq(listadetarefas)
-    elif op == 12:
-        liblistadetarefas.carregararq(listadetarefas)
-    elif op == 13:
+        print("Encerrando programa... Até logo!")
         break
     else:
-        print("\nDigite uma opção válida.")
+        print("Digite uma opção válida.")
